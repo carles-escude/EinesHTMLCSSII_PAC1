1bfb21264bba93f48918c731e58f5f947de1a791
carles-escude <75261579+carles-escude@users.noreply.github.com>
2024-10-30 21:40:48 +0100

    wip

---
+++ src/assets/scripts/app.js
@@ -0,0 +1,18 @@
+window.addEventListener("load", function(){
+    window.cookieconsent.initialise({
+      "palette": {
+        "popup": {
+          "background": "#888"
+        },
+        "button": {
+          "background": "#d4d4d4"
+        }
+      },
+      "content": {
+        "message": "Aquest lloc web utilitza galetes per assegurar que obtingui la millor experiència al nostre lloc web.",
+        "dismiss": "Accepta",
+        "link": "Més informació",
+        "href": "#"
+      }
+    });
+  });
